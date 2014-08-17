70e9da4cdebb4e830305f960dc76471f959d68b2
Gevaristo <Gevaristo@users.noreply.github.com>
2014-08-17 13:33:06 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## 
 
 makeCacheMatrix <- function(x = matrix()) {
 A<-matrix(a,nrow=y,ncol=y)
@@ -10,7 +9,6 @@ A<-matrix(a,nrow=y,ncol=y)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(A) {
 B<-solve(A)
